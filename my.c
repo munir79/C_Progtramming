ddff482b9e9de9ca560c9722f7bc303dc34fc9d1
munir79 <jakirhossainmunir79@gmail.com>
2023-10-01 00:35:51 +0600

    hey

--- my.c
+++ my.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 int main(){
-    int num;
-    printf("hey Whats Up\n");
+    int num,i;
     printf(" enter a number:");
     scanf("%d",&num);
-    printf("Your given  number is :%d",num);
-    
+    printf("Your given  number is :%d\n",num);
+    if(num>1){
+        for(i=0;i<=20;i++){
+            printf("%d\n",i);
+        }  
+    }
+    else{
+        printf("sorry");
+    }
+
+
 }
