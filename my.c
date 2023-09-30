15c5f0890eb6c2dc4055e421cf911c144f97f693
munir79 <jakirhossainmunir79@gmail.com>
2023-10-01 00:25:25 +0600

    first commit

---
+++ my.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("hey Whats Up\n");
+    printf(" enter a number:");
+    scanf("%d",&num);
+    printf("Your given  number is :%d",num);
+    
+}
